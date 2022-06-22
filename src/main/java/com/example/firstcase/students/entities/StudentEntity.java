f9a332bd3ed4ce4ec3cca40fe2671e86b31b1109
Argyn Sagash <argyn.sagash@gmail.com>
2022-06-22 11:12:57 +0600

    Create Student

---
+++ src/main/java/com/example/firstcase/students/entities/StudentEntity.java
@@ -0,0 +1,15 @@
+package com.example.firstcase.students.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class StudentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    
+
+}
